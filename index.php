f2abf9c570dcf7c3599b0868a280580049b8c7db
mvagovszky <mvagovszky@kompl.cz>
2013-08-15 17:33:19 +0200

    Tmp commit

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 #!/usr/bin/env php
 <?php
-$zf2Path = __DIR__."/vendor/zendframework/zendframework/library";
-require(__DIR__.'/init_autoloader.php');
-Zend\Mvc\Application::init(require(__DIR__.'/config/application.config.php'))->run();
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    $loader = include(__DIR__ . '/vendor/autoload.php');
+}
+if (isset($loader)) {
+    $loader->add('Zend', __DIR__ . "/vendor/zendframework/zendframework/library");
+} else {
+    include (__DIR__ . "/vendor/zendframework/zendframework/library/Zend/Loader/AutoloaderFactory.php");
+    Zend\Loader\AutoloaderFactory::factory(array(
+        'Zend\Loader\StandardAutoloader' => array(
+            'autoregister_zf' => true
+        )
+    ));
+}
+
+include('module/Application/config/module.config.php');
+include('module/Application/src/Application/Controller/IndexController.php');
+include('module/Application/Module.php');
+
+if (!class_exists('Zend\Loader\AutoloaderFactory')) {
+    throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
+}
+Zend\Mvc\Application::init(require(__DIR__ . '/config/application.config.php'))->run();
