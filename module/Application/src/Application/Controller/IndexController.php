31202a9b7d964f4a7f27974b559ab2c2863feb79
vagovszkym <vagovszky@gmail.com>
2013-08-27 21:48:39 +0200

    Execute function

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -21,6 +21,8 @@ class IndexController extends AbstractActionController
     }
     
     public function defaultAction(){
+        //$stmt = $this->getEntityManager()->getConnection()->executeQuery('SELECT FindOdd(?, ?, ?)', array(1.5,0.5,6));
+        //var_dump($stmt->fetchColumn(0));
         return "This is default console action\n";
     }
 }
