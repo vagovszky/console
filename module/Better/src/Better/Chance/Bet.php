b52ecb2861d283216df679654d3a052c61cb88c4
vagovszkym <vagovszky@gmail.com>
2013-09-01 17:30:20 +0200

    Clean better code

--- module/Better/src/Better/Chance/Bet.php
+++ module/Better/src/Better/Chance/Bet.php
@@ -7,15 +7,13 @@ class Bet {
     private $driver;
     private $login;
     private $password;
-    private $screenshots_dir;
 
     const CHANCE_URL = 'https://www.chance.cz/';
     const LIST_URL = 'https://www.chance.cz/kurzove-sazky/nabidka?obdobi=2&radit=2,1&vypisovat=2&pozadavek=vypis';
-    const WAIT_TIME = 30;
+    const WAIT_TIME = 120;
 
     public function __construct(\WebDriver $driver) {
         $this->driver = $driver;
-        $this->screenshots_dir = BASE_PATH."/data";
     }
 
     public function setPassword($password) {
@@ -29,95 +27,71 @@ class Bet {
     }
 
     public function bet($odd_id, $money) {
-        $this->driver->get(self::CHANCE_URL);
-        if (!$this->login()) return false; // log in
-        if (!$this->selectBet($odd_id)) return false; // select bet
-        if (!$this->setMoney($money)) return false; // select bet
-        if (!$this->doBet()) return false; // select bet
-        if (!$this->logout()) return false; // log out
-        return true;
+        try {
+            $this->driver->get(self::CHANCE_URL);
+            $this->login();
+            $this->selectBet($odd_id);
+            $this->setMoney($money);
+            $this->doBet();
+            $this->logout();
+            return true;
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            return false;
+        }
     }
-    
+
     public function __destruct() {
         $this->driver->close();
         $this->driver->quit();
     }
+
     // -------------------------------------------------------------------------
 
-    private function doBet(){
-        try {
-            $this->driver->findElement(\WebDriverBy::id("i_a_zaslat_tiket"))->click();
-            $this->driver->wait(self::WAIT_TIME, 1000)->until(
-                    \WebDriverExpectedCondition::presenceOfElementLocated(\WebDriverBy::className("message_ok"))
-            );
-            return true;
-        } catch (\Exception $e) {
-            $this->driver->takeScreenshot($this->screenshots_dir."/error-do-bet-screenshot.png");
-            return false;
-        }
+    private function doBet() {
+        $this->driver->findElement(\WebDriverBy::id("i_a_zaslat_tiket"))->click();
+        $this->driver->wait(self::WAIT_TIME, 1000)->until(
+                \WebDriverExpectedCondition::presenceOfElementLocated(\WebDriverBy::className("message_ok"))
+        );
     }
-    
-    private function setMoney($money){
-        try {
-            $this->driver->findElement(\WebDriverBy::name("sazka-1"))->clear();
-            $this->driver->findElement(\WebDriverBy::name("sazka-1"))->sendKeys($money);
-            $this->driver->findElement(\WebDriverBy::id("i_tiket_obsah"))->click();
-            $this->driver->wait(self::WAIT_TIME, 1000)->until(
-                    \WebDriverExpectedCondition::elementToBeClickable(\WebDriverBy::id("i_a_zaslat_tiket"))
-            );
-            return true;
-        } catch (\Exception $e) {
-            $this->driver->takeScreenshot($this->screenshots_dir."/error-set-money-screenshot.png");
-            return false;
-        }
+
+    private function setMoney($money) {
+        $this->driver->findElement(\WebDriverBy::name("sazka-1"))->clear();
+        $this->driver->findElement(\WebDriverBy::name("sazka-1"))->sendKeys($money);
+        $this->driver->findElement(\WebDriverBy::id("i_tiket_obsah"))->click();
+        $this->driver->wait(self::WAIT_TIME, 1000)->until(
+                \WebDriverExpectedCondition::elementToBeClickable(\WebDriverBy::id("i_a_zaslat_tiket"))
+        );
     }
-    
+
     private function selectBet($odd_id) {
-        try {
-            $this->driver->get(self::LIST_URL);
-            $this->driver->wait(self::WAIT_TIME, 1000)->until(
-                    \WebDriverExpectedCondition::presenceOfElementLocated(\WebDriverBy::id("souteze"))
-            );
-            $this->driver->findElement(\WebDriverBy::id("tip_$odd_id"))->click();
-            $this->driver->wait(self::WAIT_TIME, 1000)->until(
-                    \WebDriverExpectedCondition::presenceOfElementLocated(\WebDriverBy::id("i_a_zaslat_tiket"))
-            );
-            return true;
-        } catch (\Exception $e) {
-            $this->driver->takeScreenshot($this->screenshots_dir."/error-select-bet-screenshot.png");
-            return false;
-        }
+        $this->driver->get(self::LIST_URL);
+        $this->driver->wait(self::WAIT_TIME, 1000)->until(
+                \WebDriverExpectedCondition::presenceOfElementLocated(\WebDriverBy::id("souteze"))
+        );
+        $this->driver->findElement(\WebDriverBy::id("tip_$odd_id"))->click();
+        $this->driver->wait(self::WAIT_TIME, 1000)->until(
+                \WebDriverExpectedCondition::presenceOfElementLocated(\WebDriverBy::id("i_a_zaslat_tiket"))
+        );
     }
 
     private function logout() {
-        try {
-            $this->driver->findElement(\WebDriverBy::id("top_logout"))->click();
-            $this->driver->wait(self::WAIT_TIME, 1000)->until(
-                    \WebDriverExpectedCondition::alertIsPresent()
-            );
-            $this->driver->switchTo()->alert()->accept();
-            return true;
-        } catch (\Exception $e) {
-            $this->driver->takeScreenshot($this->screenshots_dir."/error-logout-screenshot.png");
-            return false;
-        }
+        $this->driver->findElement(\WebDriverBy::id("top_logout"))->click();
+        $this->driver->wait(self::WAIT_TIME, 1000)->until(
+                \WebDriverExpectedCondition::alertIsPresent()
+        );
+        $this->driver->switchTo()->alert()->accept();
     }
 
     private function login() {
-        try {
-            $this->driver->findElement(\WebDriverBy::id("ich_name"))->clear();
-            $this->driver->findElement(\WebDriverBy::id("ich_name"))->sendKeys($this->login);
-            $this->driver->findElement(\WebDriverBy::id("ich_pwd"))->clear();
-            $this->driver->findElement(\WebDriverBy::id("ich_pwd"))->sendKeys($this->password);
-            $this->driver->findElement(\WebDriverBy::id('chanceLoginButton'))->click();
-            $this->driver->wait(self::WAIT_TIME, 1000)->until(
-                    \WebDriverExpectedCondition::presenceOfElementLocated(\WebDriverBy::className("klient_info"))
-            );
-            return true;
-        } catch (\Exception $e) {
-            $this->driver->takeScreenshot($this->screenshots_dir."/error-login-screenshot.png");
-            return false;
-        }
+        $this->driver->findElement(\WebDriverBy::id("ich_name"))->clear();
+        $this->driver->findElement(\WebDriverBy::id("ich_name"))->sendKeys($this->login);
+        $this->driver->findElement(\WebDriverBy::id("ich_pwd"))->clear();
+        $this->driver->findElement(\WebDriverBy::id("ich_pwd"))->sendKeys($this->password);
+        $this->driver->findElement(\WebDriverBy::id('chanceLoginButton'))->click();
+        $this->driver->wait(self::WAIT_TIME, 1000)->until(
+                \WebDriverExpectedCondition::presenceOfElementLocated(\WebDriverBy::className("klient_info"))
+        );
     }
 
 }
