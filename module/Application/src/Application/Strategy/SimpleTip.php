f120d3eb2190fed86fcffbba0b6237f05fd0a318
mvagovszky <mvagovszky@kompl.cz>
2013-09-25 14:22:32 +0200

    Udtd simple tip

--- module/Application/src/Application/Strategy/SimpleTip.php
+++ module/Application/src/Application/Strategy/SimpleTip.php
@@ -6,6 +6,7 @@ use Better\Chance\BetInterface;
 use Doctrine\ORM\EntityManager;
 use Zend\Console\Adapter\AdapterInterface as Console;
 use BetDatabase\Entity\Tips;
+use BetDatabase\Entity\Odds;
 use Application\Options\SimpleTipOptions;
 
 class SimpleTip {
@@ -37,6 +38,8 @@ class SimpleTip {
         return $this->options;
     }
 
+    // -------------------------------------------------------------------------
+    
     private function findLastTip() {
         $query = $this->em->createQuery('SELECT t FROM BetDatabase\Entity\Tips t ORDER BY t.datetime_created DESC');
         $result = $query->setMaxResults(1)->getOneOrNullResult();
@@ -54,19 +57,23 @@ class SimpleTip {
             $stmt = $this->em->getConnection()->executeQuery('SELECT FindOdd(?, ?, ?)', array($course, $option[0], $option[1]));
             $turn = $stmt->fetchColumn(0);
             $this->console->write('Trying find odd '.$i.'. ( delta course '.$option[0].', hours +'.$option[1].' ) - ' . (empty($turn) ? '[ not found ]' : '[ ' . $turn . ' ]') . PHP_EOL);
-            if (!empty($turn)) return $turn;
+            if (!empty($turn)) return $this->em->getRepository('BetDatabase\Entity\Odds')->find($turn);
         }
-        return false;
+        return NULL;
     }
 
-    public function calculateBet($profit) {
-        $course = $this->getOptions()->getCourse();
-        return ceil($profit / ($course - 1));
+    public function calculateBet($profit, Odds $odd) {
+        $realCourse = floatval($odd->getValue());
+        if(!empty($odd)){
+            return ceil($profit / ($realCourse - 1));
+        }else{
+            return NULL;
+        }
     }
 
-    private function makeNewBet($odd_id, $bet) {
-        if(!$odd_id){
-            $this->console->write('Odd not found !!!' . PHP_EOL);
+    private function makeNewBet(Odds $odd, $bet) {
+        if(empty($odd) || empty($bet)){
+            $this->console->write('Odd not found or bad bet!!!' . PHP_EOL);
             return false;
         }
         $limit = $this->getOptions()->getLimit();
@@ -74,13 +81,12 @@ class SimpleTip {
             $this->console->write('Bet is heigher than limit ( bet - '.$bet.', limit - '.$limit.' ) !!!' . PHP_EOL);
             $result = false;
         }else{
-            $odd = $this->em->getRepository('BetDatabase\Entity\Odds')->find($odd_id);
             $match_time = $odd ? $odd->getMatch()->getDatetime()->format('d.m.Y H:i') : "unknown";
-            $this->console->write('Creating new bet with odd_id - ' . $odd_id . ' and bet is ' . $bet . ',-Kc Match starts @ [ '.$match_time.' ]' . PHP_EOL);
-            $result = $this->chance_better->bet($odd_id, $bet);        
+            $this->console->write('Creating new bet with odd_id - ' . $odd->getId() . ' and bet is ' . $bet . ',-Kc Match starts @ [ '.$match_time.' ]' . PHP_EOL);
+            $result = $this->chance_better->bet($odd->getId(), $bet);        
             if ($result) {
                 $this->console->write('Bet created successfully...' . PHP_EOL);
-                $result = $this->saveNewBet($odd_id, $bet);
+                $result = $this->saveNewBet($odd, $bet);
             } else {
                 $this->console->write('Bet creation failed, not saving !!!' . PHP_EOL);
             }
@@ -88,9 +94,8 @@ class SimpleTip {
         return $result;
     }
 
-    private function saveNewBet($odd_id, $bet) {
+    private function saveNewBet(Odds $odd, $bet) {
         try {
-            $odd = $this->em->getRepository('BetDatabase\Entity\Odds')->find($odd_id);
             $tips = new Tips();
             $tips->setBet($bet);
             $tips->setDatetimeCreated(new \DateTime());
@@ -109,26 +114,26 @@ class SimpleTip {
         $profit = $this->getOptions()->getProfit();
         $this->console->write('Making simple tip... [ '.date('d.m.Y H:i:s').' ]' . PHP_EOL);
         $last_tip = $this->findLastTip();
-        $odd_id = $this->findNewOdd();
+        $odd = $this->findNewOdd();
         if ($last_tip) {
             $last_result = $last_tip->getOdd()->getResult();
             $last_bet = $last_tip->getBet();
             switch ($last_result) {
                 case "vyhra":
                     $this->console->write('Last result was vyhra, bet was ' . $last_bet . ',-Kc' . PHP_EOL);
-                    $new_bet = $this->calculateBet($profit);
-                    $result = $this->makeNewBet($odd_id, $new_bet);
+                    $new_bet = $this->calculateBet($profit, $odd);
+                    $result = $this->makeNewBet($odd, $new_bet);
                     break;
                 case "prohra":
                     $this->console->write('Last result was prohra, bet was ' . $last_bet . ',-Kc' . PHP_EOL);
                     $profit = $last_bet + $profit;
-                    $new_bet = $this->calculateBet($profit);
-                    $result = $this->makeNewBet($odd_id, $new_bet);
+                    $new_bet = $this->calculateBet($profit, $odd);
+                    $result = $this->makeNewBet($odd, $new_bet);
                     break;
                 case "zruseno":
                     $this->console->write('Last result was zruseno, bet was ' . $last_bet . ',-Kc' . PHP_EOL);
-                    $new_bet = $this->calculateBet($profit);
-                    $result = $this->makeNewBet($odd_id, $new_bet);
+                    $new_bet = $this->calculateBet($profit, $odd);
+                    $result = $this->makeNewBet($odd, $new_bet);
                     break;
                 default:
                     $this->console->write('Last result was not been finished yet - cancel.' . PHP_EOL);
@@ -138,7 +143,7 @@ class SimpleTip {
         } else {
             $this->console->write('No tips created yet.' . PHP_EOL);
             $new_bet = $this->calculateBet($profit);
-            $result = $this->makeNewBet($odd_id, $new_bet);
+            $result = $this->makeNewBet($odd, $new_bet);
         }
         return $result;
     }
